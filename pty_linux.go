90dcd660104c37520aad9c0ce0bdb58862c0d99c
Chris Miles <chris.miles.e@gmail.com>
2026-01-09 02:10:42 -0600

    fix: linux coverage

--- pty_linux.go
+++ pty_linux.go
@@ -14,23 +14,27 @@ func ptyCheck() error {
 }
 
 func openPTY() (*os.File, *os.File, error) {
-	master, err := os.OpenFile("/dev/ptmx", os.O_RDWR|syscall.O_NOCTTY, 0)
+	return openPTYWith(os.OpenFile, ptyIoctl)
+}
+
+func openPTYWith(openFile func(string, int, os.FileMode) (*os.File, error), ioctl func(uintptr, uintptr, uintptr) error) (*os.File, *os.File, error) {
+	master, err := openFile("/dev/ptmx", os.O_RDWR|syscall.O_NOCTTY, 0)
 	if err != nil {
 		return nil, nil, err
 	}
 	fd := master.Fd()
 	unlock := int32(0)
-	if err := ptyIoctl(fd, syscall.TIOCSPTLCK, uintptr(unsafe.Pointer(&unlock))); err != nil {
+	if err := ioctl(fd, syscall.TIOCSPTLCK, uintptr(unsafe.Pointer(&unlock))); err != nil {
 		_ = master.Close()
 		return nil, nil, err
 	}
 	var ptyNum uint32
-	if err := ptyIoctl(fd, syscall.TIOCGPTN, uintptr(unsafe.Pointer(&ptyNum))); err != nil {
+	if err := ioctl(fd, syscall.TIOCGPTN, uintptr(unsafe.Pointer(&ptyNum))); err != nil {
 		_ = master.Close()
 		return nil, nil, err
 	}
 	name := fmt.Sprintf("/dev/pts/%d", ptyNum)
-	slave, err := os.OpenFile(name, os.O_RDWR|syscall.O_NOCTTY, 0)
+	slave, err := openFile(name, os.O_RDWR|syscall.O_NOCTTY, 0)
 	if err != nil {
 		_ = master.Close()
 		return nil, nil, err
